a44a34e3dba36c6d8ca2f15ffc81d2b65584ca08
Syauqan Wafiqi <siconfix@gmail.com>
2022-08-28 15:05:47 +0700

    add image

---
+++ database/migrations/2022_08_28_080243_menambah_kolom_image_table_student.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Students', function (Blueprint $table) {
+            // $table->id();
+            $table->String('image', 255)->nullable()->default('text')->after('kelas_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Students');
+    }
+};
